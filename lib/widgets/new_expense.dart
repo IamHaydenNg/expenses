13f48fea80f848c9c41cdc35a9d061f6243bbb6a
IamHaydenNg <ngheiyue.dee@gmail.com>
2024-05-16 15:10:49 +0800

    Add code of class 118: Saving New Expenses

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -62,6 +64,13 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+    widget.onAddExpense(
+      Expense(
+          amount: enteredAmount,
+          title: _titleController.text,
+          date: _selectedDate!,
+          category: _selectedCategory),
+    );
   }
 
   @override

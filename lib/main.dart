66fbe9404fcd78573874d4569b48605617dc36ff
IamHaydenNg <ngheiyue.dee@gmail.com>
2024-05-21 10:59:23 +0800

    Add code of class 124: Setting & Using Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:expenses/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 95, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: Colors.purple),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
